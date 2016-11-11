8eb29ea5ee50e4f1cf81eaea039f467b8e8814c4
Daniel Tull <dt@danieltull.co.uk>
2016-11-11 14:09:57 +0000

    Initial Commit

---
+++ ActionRouting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ActionRouting
+//
+//  Created by Daniel Tull on 11/11/2016.
+//  Copyright © 2016 Daniel Tull. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+

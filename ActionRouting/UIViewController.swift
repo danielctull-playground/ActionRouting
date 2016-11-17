7d560272a366282726a7a6c8083009c72d338594
Daniel Tull <dt@danieltull.co.uk>
2016-11-17 22:39:38 +0000

    Traverse up presented view controllers

--- ActionRouting/UIViewController.swift
+++ ActionRouting/UIViewController.swift
@@ -21,6 +21,18 @@ extension UIViewController: Router {
 			return performer.perform(action: action)
 		}
 
+		// Traverse up the presented view controller first because
+		// this will be the front-most view controller. Dismiss it
+		// if it cannot route the action.
+		if let presentedViewController = presentedViewController {
+			if presentedViewController.route(action: action) {
+				return true
+			} else {
+				dismissPresentedViewController()
+			}
+		}
+
+		// Go through all the child view controllers.
 		for viewController in childViewControllers {
 			if viewController.route(action: action) {
 				return true

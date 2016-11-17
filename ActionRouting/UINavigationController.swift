5c9e769b05576bb5b6a759d41cdb4742404db004
Daniel Tull <dt@danieltull.co.uk>
2016-11-17 22:06:08 +0000

    Prevent the scope from returning until the zero-length animation completes

--- ActionRouting/UINavigationController.swift
+++ ActionRouting/UINavigationController.swift
@@ -5,6 +5,10 @@ extension UINavigationController {
 
 	override open func show(child viewController: UIViewController) {
 		super.show(child: viewController)
+
+		CATransaction.begin()
 		popToViewController(viewController, animated: false)
+		CATransaction.commit()
+		CATransaction.flush()
 	}
 }
